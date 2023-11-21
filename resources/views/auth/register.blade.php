88f9b1d2462274a096cbf12bbe9d602ebb3562c9
HINAIKI <naikibro@gmail.com>
2023-11-21 09:24:13 -1000

    feat: Register page
    - Customized background
    - overrided the Breeze template for registration
    
    mockup: https://xd.adobe.com/view/bb7ff8aa-68b8-483f-8071-c75a77806974-ee2c/?fullscreen&hints=off
    
    ref: https://app.clickup.com/t/86ctqw5q4

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,66 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html data-theme="dark" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>A hoe tatou</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/ressources/icon.png') }}">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="{{asset('css/register.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body class="bg-cover bg-center flex items-center min-h-screen" style="background-image: url('{{ asset('img/vaa.jpg') }}')">
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+<nav>
+</nav>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+<a class="mt-4" href="{{ route('welcome') }}">
+    <img alt="icon" src="{{ asset('img/ressources/icon.png') }}" class="logoImg">
+</a>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+<form method="POST" action="{{ route('register') }}" class="bg-white dark:bg-gray-800 p-8 rounded-md shadow-md max-w-md w-full mx-auto mt-4">
+    @csrf
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <h2 class="mb-4">Create a new account</h2>
+    <!-- Name -->
+    <div class="mb-4">
+        <x-input-label for="name" :value="__('Name')" />
+        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+    </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+    <!-- Email Address -->
+    <div class="mb-4">
+        <x-input-label for="email" :value="__('Email')" />
+        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+    <!-- Password -->
+    <div class="mb-4">
+        <x-input-label for="password" :value="__('Password')" />
+        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+    </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+    <!-- Confirm Password -->
+    <div class="mb-4">
+        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+        <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+    </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+    <div class="flex items-center justify-between mt-4">
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+            {{ __('Already registered?') }}
+        </a>
 
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <x-primary-button>
+            {{ __('Register') }}
+        </x-primary-button>
+    </div>
+</form>
+
+</body>
+</html>
