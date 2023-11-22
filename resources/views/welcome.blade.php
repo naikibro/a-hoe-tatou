fbb15646a1a0ccdc09dd436e02e6b6c72c52f6b7
naikibro <naikibro@gmail.com>
2023-11-21 22:21:33 -1000

    fix: dynamise the authentication frontend
    - added mediaqueries for mobile responsivity
    - added animations
    - resized logo
    - added favicon
    - added pages titles
    
    ref: https://github.com/naikibro/a-hoe-tatou/pull/2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,17 +10,11 @@
             body{
                 background-image: url("{{asset('img/vaa.jpg')}}");
             }
-
-            .application-logo{
-                width: 32px;
-                height: 32px;
-            }
         </style>
 
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
         <link href="{{asset('common.css')}}" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
 
     </head>
     <body>
@@ -40,6 +34,7 @@
                             <hr>
                             <br>
                             @if (Route::has('register'))
+                                <h2 class="mb-4 text-white">Or <b>create a new account !</b></h2>
                                 <a class="inline-flex items-center px-4 py-2 bg-green-500 dark:bg-green-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-green-600 dark:hover:bg-white focus:bg-green-600 dark:focus:bg-white active:bg-green-700 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 w-full mt-1 flex items-center justify-center" href="{{ route('register') }}">Register</a>
                             @endif
                         @endauth
@@ -48,7 +43,7 @@
             </div>
 
             <div id="heroBox" class="mt-4 sm:mt-8 md:mt-12 lg:mt-16 xl:mt-20">
-                <a href="#">
+                <a href="{{ route('welcome') }}">
                     <img alt="icon" src="{{ asset('img/ressources/icon.png') }}" class="logoImg">
                 </a>
             </div>
