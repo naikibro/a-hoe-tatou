8793b72d2da9da281a61ca98afb87ddb4a3548c9
naikibro <naikibro@gmail.com>
2023-11-26 18:05:24 -1000

    feat: adding test Mailer
    - added credentials for webmastering mailer in envfile
    - created POC mail and controller
    - added routing for mail testing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MailController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -30,6 +31,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware('role:admin')->group(function () {
+        // Admin-specific logic
+        Route::get('/send-mail', [MailController::class, 'sendHelloWorldEmail']);
+    });
 });
 
 Route::get('/login', function () {
@@ -43,7 +49,5 @@ Route::get('/register', function () {
 Route::get('/about-data-protection', function () {
     return view('auth.about');
 })->name('about-data-protection');
-
-
 
 require __DIR__.'/auth.php';
