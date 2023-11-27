9786e33b1fae85e611e45953b490cfe1648c119b
naikibro <naikibro@gmail.com>
2023-11-26 18:19:30 -1000

    feat: added role for User
    - created roles for users
    - default role is 'user'
    - added default admin creation in migration
    - stored admin credentials in env file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -17,9 +19,23 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // Create an admin user using environment variables
+        $adminEmail = env('ADMIN_EMAIL', 'admin@example.com');
+        $adminPassword = env('ADMIN_PASSWORD', 'password');
+
+        DB::table('users')->insert([
+            'name' => 'Admin User',
+            'email' => $adminEmail,
+            'password' => Hash::make($adminPassword),
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
