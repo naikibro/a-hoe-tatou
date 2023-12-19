d4ba6b4e1b8138970c59012635debad85ef4c958
naikibro <naikibro@gmail.com>
2023-12-18 23:48:03 -1000

    fix: trying to fix the script
    - removed trash
    - put run dev at the end

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -9,36 +9,17 @@ if [ ! -d "$PROJECT_PATH" ]; then
     exit 1
 fi
 
-# Source the .env file to load environment variables
-source "$PROJECT_PATH"/.env
-
-# Check if the required environment variables are set
-if [ -z "$DB_DATABASE" ] || [ -z "$DB_USERNAME" ] || [ -z "$DB_PASSWORD" ]; then
-    echo "Error: Database environment variables not set in .env"
-    exit 1
-fi
-
-# Check if the database exists
-if [ -z "$(mysql -u $DB_USERNAME -p$DB_PASSWORD -e "SHOW DATABASES LIKE '$DB_DATABASE';" 2>/dev/null)" ]; then
-    echo "Creating database $DB_DATABASE..."
-    mysql -u $DB_USERNAME -p$DB_PASSWORD -e "CREATE DATABASE $DB_DATABASE;"
-    echo "Database $DB_DATABASE created!"
-fi
-
-
-composer install --working-dir="$PROJECT_PATH"
-npm install --prefix "$PROJECT_PATH"
-npm run dev
-
-
 # Stop services
 "$PROJECT_PATH"/vendor/bin/sail down
 
-# Clear PHP cache
-"$PROJECT_PATH"/vendor/bin/sail artisan optimize:clear
-
 # Start services in detached mode
 "$PROJECT_PATH"/vendor/bin/sail up -d
+
+composer install --working-dir="$PROJECT_PATH"
+npm install --prefix "$PROJECT_PATH"
+
+# Clear PHP cache
+"$PROJECT_PATH"/vendor/bin/sail artisan optimize:clear
 
 # Run migrations
 "$PROJECT_PATH"/vendor/bin/sail artisan migrate:fresh
@@ -47,3 +28,5 @@ npm run dev
 "$PROJECT_PATH"/vendor/bin/sail artisan db:seed --class=UserbaseDev
 
 echo "Laravel project setup complete!"
+npm run dev
+
